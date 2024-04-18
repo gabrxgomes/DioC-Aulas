a02158696c208d1d28e603782505a5a645136f87
Gabriel Gomes <100102446+gabrxgomes@users.noreply.github.com>
2024-04-18 16:46:34 -0300

    Enviando projeto da aula de contrutores e metodos

---
+++ ExemploExplorando/Models/Pessoa.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploExplorando.Models
+{
+    public class Pessoa
+    {
+        //vamos declarar os get e set da classe
+        //prop gera automatico
+        //public int MyProperty { get; set; }
+        //mude para desable a ultima propriedade no .csproj para tirar o erro de nulo
+        // public string Nome {get; set;}
+
+        private string _nome; //faz referencia ao meu Nome
+        // como uma variavel que armazena o valor do nome para modifica-lo e usa-lo
+
+        private int _idade;
+        public string Nome
+        {
+            // get //recebe valor
+            // {
+            //     return _nome.ToUpper();
+            // }
+
+            get => _nome.ToUpper(); //podemos resumir para se usar assim, uma body expression
+            //maneira mais sucinta e performatica de se fazer o mesmo de cima
+            //TRATAMENTO DOS DADOS
+
+            set //atribui valor
+            {
+                if (value == "")
+                {
+                    throw new ArgumentException("O nome não pode ser vazio");
+                    //tratamento de excessão
+                }
+
+                _nome = value;
+            }
+        }
+        public string Sobrenome { get; set; } //propriedade
+
+        public string NomeCompleto => $"{Nome} {Sobrenome}".ToUpper();
+        //propriedade
+            
+
+
+
+        public int Idade
+        {
+            get => _idade; //não tem a necessidade de se tratar os dados igual acontece com string
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("A idade não pode ser menor que zero");
+                }
+                _idade = value;
+            }
+        }
+
+
+        public void Apresentar(){
+            Console.WriteLine($"Olá meu nome é, {NomeCompleto}, e a minha idade é {Idade}");
+        }
+        
+    }
+}
---
+++ ExemploExplorando/Program.cs
@@ -0,0 +1,31 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using ExemploExplorando.Models;
+
+
+
+// pessoa1.Nome = "José";
+// pessoa1.Sobrenome = "Carvalho";
+// pessoa1.Idade = 50;
+// pessoa1.Apresentar();
+
+Pessoa pessoa1 = new Pessoa();
+pessoa1.Nome = "Lucas";
+pessoa1.Sobrenome = "Silva";
+
+
+Pessoa pessoa2 = new Pessoa();
+pessoa2.Nome = "Carlos";
+pessoa2.Sobrenome = "Fernandes";
+
+
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome =  "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+
+cursoDeIngles.AdicionarAluno(pessoa1);
+cursoDeIngles.AdicionarAluno(pessoa2);
+cursoDeIngles.ListarAlunos();
