5b9a6ace4a7320c0d243d5daee7d94213a217194
Gabriel nascimento <gabrielnasciment123@hotmail.com>
2024-05-04 11:37:36 -0300

    enviando alterações

--- ExemploExplorando/Program.cs
+++ ExemploExplorando/Program.cs
@@ -2,6 +2,7 @@
 
 
 CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt_BR");
+//use this if you are completly decided that your code in full culture
 
 
 
@@ -11,5 +12,8 @@ decimal valorMonetario = 1582.40M;
 
 //Console.WriteLine($"{valorMonetario:C}");
 
-Console.WriteLine(valorMonetario.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
+Console.WriteLine(valorMonetario.ToString("C2", CultureInfo.CreateSpecificCulture("en-US")));
+
+//case i use just C or C2 set my value in 2 decimal cases, just C1 one deciamal case
+
 
