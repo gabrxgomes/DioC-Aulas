c982de3dc18ba564a48bd8ea1468d4bfd52f0238
Gabriel nascimento <gabrielnasciment123@hotmail.com>
2024-05-10 21:44:20 -0300

    eviando o inicio de abstração

--- ExemploPoo/Program.cs
+++ ExemploPoo/Program.cs
@@ -1,9 +1,9 @@
 ﻿using ExemploPoo.Models;
 
+//inicio poo abstração
+// Pessoa p1 = new Pessoa();
 
-Pessoa p1 = new Pessoa();
+// p1.Nome = "John";
+// p1.Idade = 20;
 
-p1.Nome = "John";
-p1.Idade = 20;
-
-p1.Apresentar();
+// p1.Apresentar();
