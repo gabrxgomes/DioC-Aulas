e903858d518f24d6d0e3c4441784ae3d3946f753
Gabriel nascimento <gabrielnasciment123@hotmail.com>
2024-05-10 21:33:58 -0300

    adicionando a folder de poo

--- ExemploExplorando/Models/Venda1.cs
+++ ExemploExplorando/Models/Venda1.cs
@@ -2,12 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+//adicionamos o nosso pacote do nuget
+using Newtonsoft.Json;
 
 namespace ExemploExplorando.Models
 {
     public class Venda1
     {
+
+        //caso o nosso json venha com seu identificador diferente do padrao do
+        //nome da nossa variavel na classe como nesse aqui, podemos 
+        //adicionar um atributo a nossa leitura JSON
         public int Id { get; set; }
+
+        [JsonProperty("Nome_Produto")]//passmos esse nome caso o nome do atributo no json seja diferente do
+        //que nossas variaveis aqui !
         public string Produto { get; set; }
         public decimal Preco { get; set; }
         public DateTime DataVenda { get; set; }
---
+++ ExemploPoo/Program.cs
@@ -0,0 +1,9 @@
+﻿using ExemploPoo.Models;
+
+
+Pessoa p1 = new Pessoa();
+
+p1.Nome = "John";
+p1.Idade = 20;
+
+p1.Apresentar();
